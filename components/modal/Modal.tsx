100f25df593b7e2db5caaba742e24077f7a33e3a
binary64 <1680627+binary64@users.noreply.github.com>
2021-01-19 12:39:44 +0000

    make Modal animate to/from the center of the screen

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -10,27 +10,7 @@ import { LegacyButtonType, ButtonProps, convertLegacyProps } from '../button/but
 import LocaleReceiver from '../locale-provider/LocaleReceiver';
 import { ConfigContext, DirectionType } from '../config-provider';
 
-let mousePosition: { x: number; y: number } | null;
 export const destroyFns: Array<() => void> = [];
-
-// ref: https://github.com/ant-design/ant-design/issues/15795
-const getClickPosition = (e: MouseEvent) => {
-  mousePosition = {
-    x: e.pageX,
-    y: e.pageY,
-  };
-  // 100ms 内发生过点击事件，则从点击位置动画展示
-  // 否则直接 zoom 展示
-  // 这样可以兼容非点击方式展开
-  setTimeout(() => {
-    mousePosition = null;
-  }, 100);
-};
-
-// 只有点击事件支持从鼠标位置动画展开
-if (typeof window !== 'undefined' && window.document && window.document.documentElement) {
-  document.documentElement.addEventListener('click', getClickPosition, true);
-}
 
 export interface ModalProps {
   /** 对话框是否可见 */
@@ -208,7 +188,6 @@ const Modal: ModalInterface = props => {
       wrapClassName={wrapClassNameExtended}
       footer={footer === undefined ? defaultFooter : footer}
       visible={visible}
-      mousePosition={mousePosition}
       onClose={handleCancel}
       closeIcon={closeIconToRender}
       focusTriggerAfterClose={focusTriggerAfterClose}
